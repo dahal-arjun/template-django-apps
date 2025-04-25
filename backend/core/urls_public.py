719f166c80fa2d506d536fe12bec1c3293d27d6a
Arjun Dahal <devapp864@gmail.com>
2025-04-25 21:16:50 +0545

    Update urls_public.py

--- backend/core/urls_public.py
+++ backend/core/urls_public.py
@@ -30,9 +30,11 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path("", include("tasks.urls")),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
     ]
